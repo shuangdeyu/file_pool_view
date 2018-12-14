2faa4db43b7a39aeaca0451c876621d7d734e427
jiangyj <jiangyj@zjmax.com>
2018-12-14 16:37:53 +0800

    交互形式架构修改

--- static/js/common.js
+++ static/js/common.js
@@ -11,6 +11,7 @@ const ACTION_LOGIN = 100; // 登录 100
 const ACTION_REGISTER = 101; // 注册 101
 const ACTION_LOGIN_OUT = 102; // 退出登录 102
 const ACTION_LUSER_INFO = 103; // 用户信息 103
+const ACTION_CHECK_TOKEN_LOGIN = 400; // 检查客户端登录状态 400
 
 // 状态码
 const RES_SUCCESS = 0; // 成功
@@ -27,107 +28,117 @@ function now_time() {
  * 获取token
  */
 function get_token() {
+    let defer = $.Deferred();
     let storage = window.localStorage;
     let token = storage.getItem("token") ? storage.getItem("token") : "";
-    if (token) {
-        return token
+    if (token != "") {
+        defer.resolve(token);
     } else {
         $.ajax({
             type: "post",
             url: TokenUrl,
             dataType: "json",
-            async: false,
+            // async: false,
             data: {},
-            success: function (res) {
-                if (res["e"] == RES_SUCCESS) {
-                    token = res["data"];
-                }
-            },
+        }).done(function (res) {
+            if (res["e"] == RES_SUCCESS) {
+                token = res["data"];
+                storage.setItem("token", token);
+                defer.resolve(token);
+            }
         });
-        storage.setItem("token", token);
     }
-    return token
+    return defer.promise();
 }
 
 /**
  * 获取encrypt
  */
 function get_encrypt(action, timestamp) {
+    let defer = $.Deferred();
     let encrypt = "";
     $.ajax({
         type: "post",
         url: EncryptUrl,
         dataType: "json",
-        async: false,
+        // async: false,
         data: {
             action: action,
             timestamp: timestamp,
         },
-        success: function (res) {
-            if (res["e"] == RES_SUCCESS) {
-                encrypt = res["data"];
-            }
-        },
+    }).done(function (res) {
+        if (res["e"] == RES_SUCCESS) {
+            encrypt = res["data"];
+            defer.resolve(encrypt);
+        }
     });
-    return encrypt
+    return defer.promise();
 }
 
 /*
- * 获取sing
+ * 获取sign
  */
-function get_singn(param) {
+function get_sign(param) {
+    let defer = $.Deferred();
     let sign = "";
     $.ajax({
         type: "post",
         url: SignUrl,
         dataType: "json",
-        async: false,
+        // async: false,
         data: {
             param: param,
         },
-        success: function (res) {
-            if (res["e"] == RES_SUCCESS) {
-                sign = res["data"];
-            }
-        },
+    }).done(function (res) {
+        if (res["e"] == RES_SUCCESS) {
+            sign = res["data"];
+            defer.resolve(sign);
+        }
     });
-    return sign
+    return defer.promise();
 }
 
 /*
  * 加密参数
  */
 function encrypt_param(action, param) {
-    let timestamp = now_time();
-    // 获取token
-    let token = get_token();
-    // 获取encrypt
-    let encrypt = get_encrypt(action, timestamp);
-    // 获取sing
-    param += "&timestamp=" + timestamp;
-    let sign = get_singn(param);
-    // 加密参数
-    param += "&sign=" + sign;
-    let en_param = "";
-    $.ajax({
-        type: "post",
-        url: EncryptParamUrl,
-        dataType: "json",
-        async: false,
-        data: {
-            param: param,
-        },
-        success: function (res) {
-            if (res["e"] == RES_SUCCESS) {
-                en_param = res["data"];
-            }
-        },
-    });
+    let defer = $.Deferred();
 
-    return {
-        "token": token,
-        "encrypt": encrypt,
-        "sign": sign,
-        "param": en_param,
-    };
+    let timestamp = now_time();
+    let token = "";
+    let encrypt = "";
+    let sign = "";
+    let en_param = "";
+    if (param == "") {
+        param = "timestamp=" + timestamp;
+    } else {
+        param += "&timestamp=" + timestamp;
+    }
+    $.when(get_token(), get_encrypt(action, timestamp), get_sign(param))
+        .done(function (d1, d2, d3) {
+            token = d1;
+            encrypt = d2;
+            sign = d3;
+            param += "&sign=" + d3;
+            $.ajax({
+                type: "post",
+                url: EncryptParamUrl,
+                dataType: "json",
+                // async: false,
+                data: {
+                    param: param,
+                },
+            }).done(function (res) {
+                if (res["e"] == RES_SUCCESS) {
+                    en_param = res["data"];
+                }
+                defer.resolve({
+                    "token": token,
+                    "encrypt": encrypt,
+                    "sign": sign,
+                    "param": en_param,
+                });
+            });
+        });
+    return defer.promise();
 }
--- static/js/judge_login.js
+++ static/js/judge_login.js
@@ -4,3 +4,26 @@ token = storage.getItem("token") ? storage.getItem("token") : "";
 if (token == "") {
     window.location = "login.html"
 }
+// 检查服务器是否登录状态
+$.when(encrypt_param(ACTION_CHECK_TOKEN_LOGIN, "")).done(function (data) {
+    console.log(data);
+    $.ajax({
+        type: "post",
+        url: AppUrl,
+        dataType: "json",
+        data: {
+            action: ACTION_CHECK_TOKEN_LOGIN,
+            token: data.token,
+            encrypt: data.encrypt,
+            param: data.param,
+        },
+    }).done(function (res) {
+        if (res["e"] != 0) {
+            alert(res["msg"]);
+        } else {
+            if (res["data"]["is_login"] != 1) {
+                window.location = "login.html"
+            }
+        }
+    });
+});
